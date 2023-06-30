b5baa908c38932ff7140f1945f0a503c00aaedb7
Alexander Markin <alexander.markin@creamteam.de>
2023-06-30 13:38:08 +0200

    shopping list

--- recipe-list/src/app/app.module.ts
+++ recipe-list/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { RecipesListComponent } from './recipes/recipes-list/recipes-list.compon
     RecipesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
