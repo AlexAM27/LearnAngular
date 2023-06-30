6393219e39390f827e18012a60197f69512274c7
Alexander Markin <alexander.markin@creamteam.de>
2023-06-30 14:34:01 +0200

    img

--- recipe-list/src/app/recipes/recipes-list/recipes-list.component.ts
+++ recipe-list/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -29,7 +29,7 @@ export class RecipesListComponent {
 
   newRecipe1: Recipe = {
     id: 1,
-    image: '../../../images/recipe-img.jpg',
+    image: 'assets/images/recipe-image.jpg',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis neque augue. Nullam sed tortor congue lorem vulputate congue ac sed massa. Donec posuere felis risus, sed efficitur massa interdum vehicula. Vivamus quis metus quis metus fringilla malesuada. Praesent eu risus a nulla pretium blandit in ac nunc. Sed et interdum dui, eu laoreet leo. Curabitur lacus mauris, volutpat at magna eu, viverra lobortis sapien. Integer dignissim, tortor quis ultrices mattis, sem est aliquet diam, in euismod magna diam vel libero. Aliquam in imperdiet nisl. Duis lacinia pretium quam, pharetra facilisis ex consequat eget. Suspendisse iaculis tempus libero, at auctor ex vestibulum in. Donec egestas mollis sollicitudin.',
     ingredient: [this.ingr],
     name: 'Beer'
@@ -37,7 +37,7 @@ export class RecipesListComponent {
 
   newRecipe2: Recipe = {
     id: 2,
-    image: '../../../images/recipe-img.jpg',
+    image: 'assets/images/recipe-image.jpg',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis neque augue. Nullam sed tortor congue lorem vulputate congue ac sed massa. Donec posuere felis risus, sed efficitur massa interdum vehicula. Vivamus quis metus quis metus fringilla malesuada. Praesent eu risus a nulla pretium blandit in ac nunc. Sed et interdum dui, eu laoreet leo. Curabitur lacus mauris, volutpat at magna eu, viverra lobortis sapien. Integer dignissim, tortor quis ultrices mattis, sem est aliquet diam, in euismod magna diam vel libero. Aliquam in imperdiet nisl. Duis lacinia pretium quam, pharetra facilisis ex consequat eget. Suspendisse iaculis tempus libero, at auctor ex vestibulum in. Donec egestas mollis sollicitudin.',
     ingredient: [this.ingr],
     name: 'Beer'
