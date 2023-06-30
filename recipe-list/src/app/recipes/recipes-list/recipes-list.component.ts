132a23d7d003e6a0569acabf32996f92d5b0ca24
Alexander Markin <alexander.markin@creamteam.de>
2023-06-30 14:03:24 +0200

    list

--- recipe-list/src/app/recipes/recipes-list/recipes-list.component.ts
+++ recipe-list/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -22,15 +22,30 @@ interface Recipe {
 
 export class RecipesListComponent {
 
-  // const newRecipe: Recipe = {
-  //   id: 1,
-  //   image: '../../../images/recipe-img.jpg',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis neque augue. Nullam sed tortor congue lorem vulputate congue ac sed massa. Donec posuere felis risus, sed efficitur massa interdum vehicula. Vivamus quis metus quis metus fringilla malesuada. Praesent eu risus a nulla pretium blandit in ac nunc. Sed et interdum dui, eu laoreet leo. Curabitur lacus mauris, volutpat at magna eu, viverra lobortis sapien. Integer dignissim, tortor quis ultrices mattis, sem est aliquet diam, in euismod magna diam vel libero. Aliquam in imperdiet nisl. Duis lacinia pretium quam, pharetra facilisis ex consequat eget. Suspendisse iaculis tempus libero, at auctor ex vestibulum in. Donec egestas mollis sollicitudin.',
-  //   ingredient
+  ingr: Ingredient = {
+    name: 'Beer',
+    amount: 4
+  }
 
-  // }
+  newRecipe1: Recipe = {
+    id: 1,
+    image: '../../../images/recipe-img.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis neque augue. Nullam sed tortor congue lorem vulputate congue ac sed massa. Donec posuere felis risus, sed efficitur massa interdum vehicula. Vivamus quis metus quis metus fringilla malesuada. Praesent eu risus a nulla pretium blandit in ac nunc. Sed et interdum dui, eu laoreet leo. Curabitur lacus mauris, volutpat at magna eu, viverra lobortis sapien. Integer dignissim, tortor quis ultrices mattis, sem est aliquet diam, in euismod magna diam vel libero. Aliquam in imperdiet nisl. Duis lacinia pretium quam, pharetra facilisis ex consequat eget. Suspendisse iaculis tempus libero, at auctor ex vestibulum in. Donec egestas mollis sollicitudin.',
+    ingredient: [this.ingr],
+    name: 'Beer'
+  }
+
+  newRecipe2: Recipe = {
+    id: 2,
+    image: '../../../images/recipe-img.jpg',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis neque augue. Nullam sed tortor congue lorem vulputate congue ac sed massa. Donec posuere felis risus, sed efficitur massa interdum vehicula. Vivamus quis metus quis metus fringilla malesuada. Praesent eu risus a nulla pretium blandit in ac nunc. Sed et interdum dui, eu laoreet leo. Curabitur lacus mauris, volutpat at magna eu, viverra lobortis sapien. Integer dignissim, tortor quis ultrices mattis, sem est aliquet diam, in euismod magna diam vel libero. Aliquam in imperdiet nisl. Duis lacinia pretium quam, pharetra facilisis ex consequat eget. Suspendisse iaculis tempus libero, at auctor ex vestibulum in. Donec egestas mollis sollicitudin.',
+    ingredient: [this.ingr],
+    name: 'Beer'
+  }
+
   recipesList: Recipe[] = [
-   
+    this.newRecipe1,
+    this.newRecipe2,
   ];
 
 
