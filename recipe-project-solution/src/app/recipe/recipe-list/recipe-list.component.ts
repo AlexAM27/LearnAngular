13a6d1b0295675fdfb4e14afbee0c1207782de21
Alexander Markin <alexander.markin@creamteam.de>
2023-07-05 14:47:34 +0200

    recepie list

--- recipe-project-solution/src/app/recipe/recipe-list/recipe-list.component.ts
+++ recipe-project-solution/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,5 +8,10 @@ import { Component } from "@angular/core";
 })
 
 export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe('Test', 'simple test', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F5d85474013c270d16accdcca47f84297%2F168743755530420230621_144224.jpg&q=60&c=sc&orient=true&poi=auto&h=512'),
+    new Recipe('Test', 'simple test', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F5d85474013c270d16accdcca47f84297%2F168743755530420230621_144224.jpg&q=60&c=sc&orient=true&poi=auto&h=512')
+  ]
 
 }
