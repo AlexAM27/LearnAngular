1d48db981dd1b742494a0c988238a0bdb14e6e97
Alexander Markin <alexander.markin@creamteam.de>
2023-09-01 14:14:52 +0200

    angular

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,7 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
 
-  newServerName = '';
+  // newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -18,12 +18,12 @@ export class CockpitComponent implements OnInit {
   }
 
 
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddServer(serverName: HTMLInputElement) {
+    this.serverCreated.emit({serverName: serverName.value, serverContent: this.newServerContent});
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({blueprintName: this.newServerName, blueprintContent: this.newServerContent});
+  onAddBlueprint(serverName: HTMLInputElement) {
+    this.blueprintCreated.emit({blueprintName: serverName.value, blueprintContent: this.newServerContent});
   }
 
 }
