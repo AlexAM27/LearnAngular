099ecc95f74f10105355d22ab970adea86ff8a8c
Alexander Markin <alexander.markin@creamteam.de>
2023-09-04 13:51:07 +0200

    viewcontent

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from "@angular/core";
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ContentChild, DoCheck, ElementRef, Input, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from "@angular/core";
 
 @Component({
   selector:'app-server-element',
@@ -9,6 +9,8 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   // @Input('inputElement') element: {type: string, name: string, content: string};
   @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
   constructor(){
     console.log('constructor called!');
@@ -21,6 +23,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
 
   ngOnInit() {
     console.log('ngOnInit called!');
+    // console.log(this.header.nativeElement.textContent)
   }
 
   ngDoCheck() {
@@ -29,6 +32,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
 
   ngAfterContentInit() {
     console.log('ngAfterContentInit called!');
+    console.log(this.paragraph.nativeElement.textContent);
   }
 
   ngAfterContentChecked(): void {
@@ -37,6 +41,7 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
 
   ngAfterViewInit() {
     console.log('AfterViewInit called!');
+    console.log(this.header.nativeElement.textContent)
   }
 
   ngAfterViewChecked(): void {
