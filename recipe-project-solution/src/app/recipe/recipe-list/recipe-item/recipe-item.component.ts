603832d9e45c788e31fc38200b6b0a192866caad
Alexander Markin <alexander.markin@creamteam.de>
2023-09-06 16:38:55 +0200

    recipedetails

--- recipe-project-solution/src/app/recipe/recipe-list/recipe-item/recipe-item.component.ts
+++ recipe-project-solution/src/app/recipe/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { Recipe } from "../../recipe.model";
 
 @Component({
@@ -8,6 +8,13 @@ import { Recipe } from "../../recipe.model";
 })
 
 export class RecipeItemComponent {
+
   @Input() item: Recipe;
+
+  @Output() recipeItem = new EventEmitter<void>();
+
+  onRecipeItemClick() {
+    this.recipeItem.emit();
+  }
 
 }
--- recipe-project-solution/src/app/recipe/recipe-list/recipe-list.component.ts
+++ recipe-project-solution/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -11,7 +11,13 @@ export class RecipeListComponent {
 
   recipes: Recipe[] = [
     new Recipe('Test', 'simple test', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F5d85474013c270d16accdcca47f84297%2F168743755530420230621_144224.jpg&q=60&c=sc&orient=true&poi=auto&h=512'),
-    new Recipe('Test', 'simple test', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F5d85474013c270d16accdcca47f84297%2F168743755530420230621_144224.jpg&q=60&c=sc&orient=true&poi=auto&h=512')
+    new Recipe('Test 2', 'simple test 2', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2F5d85474013c270d16accdcca47f84297%2F168743755530420230621_144224.jpg&q=60&c=sc&orient=true&poi=auto&h=512')
   ]
+
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  onRecipeSelected(item: Recipe) {
+    this.recipeSelected.emit(item);
+  }
 
 }
