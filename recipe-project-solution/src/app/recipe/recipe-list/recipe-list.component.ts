50ad9619697d30d1a749665b0d82daaddae600dc
AlexAM27 <alexandermarkin27@gmail.com>
2023-09-12 21:41:54 +0200

    recipe service

--- recipe-project-solution/src/app/recipe/recipe-list/recipe-list.component.ts
+++ recipe-project-solution/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -14,11 +14,5 @@ export class RecipeListComponent implements OnInit {
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  @Output() recipeSelected = new EventEmitter<Recipe>();
-
-  onRecipeSelected(item: Recipe) {
-    this.recipeSelected.emit(item);
   }
 }
