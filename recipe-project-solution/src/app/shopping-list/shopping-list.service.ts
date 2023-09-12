bfa93a39b1634e24d40da2fd642000065c1ef5dd
AlexAM27 <alexandermarkin27@gmail.com>
2023-09-12 21:59:58 +0200

    shopping list service

--- recipe-project-solution/src/app/shopping-list/shopping-list.service.ts
+++ recipe-project-solution/src/app/shopping-list/shopping-list.service.ts
@@ -1,1 +1,19 @@
-export class ShoppingListService {}
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatos', 10),
+  ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addNewIngredient(item: Ingredient) {
+    this.ingredients.push(item);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+}
