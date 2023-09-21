129bb43b3169029f1556469c28c66fa49503d748
Alexander Markin <alexander.markin@creamteam.de>
2023-09-21 13:40:32 +0200

    finish routes

--- recipe-project-solution/src/app/app-routing.module.ts
+++ recipe-project-solution/src/app/app-routing.module.ts
@@ -3,13 +3,18 @@ import { RouterModule, Routes } from "@angular/router";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipiesComponent } from "./recipe/recipies.component";
 import { RecipeDetailComponent } from "./recipe/recipe-detail/recipe-detail.component";
+import { RecipeStartComponent } from "./recipe/recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipe/recipe-edit/recipe-edit.component";
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipies', pathMatch: "full"},
   { path: 'recipies',
     component: RecipiesComponent,
     children: [
-      { path: ':name', component: RecipeDetailComponent}
+      { path: '', component: RecipeStartComponent, pathMatch: "full" },
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':name', component: RecipeDetailComponent },
+      { path: ':name/edit', component: RecipeEditComponent },
     ]
   },
   {path: 'shopping-list', component: ShoppingListComponent},
--- recipe-project-solution/src/app/app.module.ts
+++ recipe-project-solution/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { DropdownDirective } from './shared/dropdown-directive';
 import { RecipeService } from './recipe/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +29,8 @@ import { AppRoutingModule } from './app-routing.module';
     RecipiesComponent,
     ShoppingEditComponent,
     DropdownDirective,
+    RecipeStartComponent,
+    RecipeEditComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [RecipeService, ShoppingListService],
